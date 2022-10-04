8e313856e6a03e0e9dd752f786f4bbff42892bce
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-10-04 20:40:59 -0300

    Requisito 5.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisable: true,
+
+    };
+  }
+
+  handleChange = (e) => {
+    const artistName = e.target.value;
+    const minCaracter = 2;
+    this.setState({ isDisable: (artistName.length < minCaracter) });
+  };
+
   render() {
+    const { name, isDisable } = this.state;
+
     return (
       <div data-testid="page-search">
         <p>Search</p>
         <Header />
+        <label htmlFor="name" placeholder="Nome do artista">
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ name }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ isDisable }
+          data-testid="search-artist-button"
+          // onClick={ this.savingUser }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
