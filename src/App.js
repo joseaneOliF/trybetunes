6cee84ca8df40112c1bdb8faf9b545693fd8423f
Joseane Oliveira <joseaneoliveira.joli@gmail.com>
2022-10-03 21:51:20 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
@@ -11,16 +11,16 @@ import Search from './pages/Search';
 class App extends React.Component {
   render() {
     return (
-      <>
+      <BrowserRouter>
         <p>TrybeTunes</p>
-        <Route path="/" component={ Login } />
+        <Route exact path="/" component={ Login } />
         <Route path="/search" component={ Search } />
         <Route path="/album/:id" component={ Album } />
         <Route path="/favorites" component={ Favorites } />
         <Route path="/profile" component={ Profile } />
         <Route path="/profile/edit" component={ ProfileEdit } />
         <Route path="*" component={ NotFound } />
-      </>
+      </BrowserRouter>
     );
   }
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,75 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../services/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      isDisable: true,
+      isLoading: false,
+      savingName: false,
+    };
+  }
+
+  handleChange = (e) => {
+    const nome = e.target.value;
+    const minCaracter = 3;
+    if (nome.length >= minCaracter) {
+      this.setState({
+        name: nome,
+        isDisable: false,
+      });
+    }
+    if (nome.length < minCaracter) {
+      this.setState({
+        name: nome,
+        isDisable: true,
+      });
+    }
+  };
+
+  savingUser = async () => {
+    const { name } = this.state;
+    this.setState({ isLoading: true });
+    await createUser({ name });
+    this.setState({
+      savingName: true,
+    });
+  };
+
   render() {
+    const { name, isDisable, isLoading, savingName } = this.state;
     return (
-      <div data-testid="page-login">
-        <p>Login</p>
-      </div>
+      <>
+        {savingName && <Redirect to="/search" />}
+        {isLoading ? <Loading />
+          : (
+
+            <div data-testid="page-login">
+              <p>Login</p>
+              <label htmlFor="name">
+                Nome
+                <input
+                  type="text"
+                  data-testid="login-name-input"
+                  value={ name }
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <button
+                type="submit"
+                disabled={ isDisable }
+                data-testid="login-submit-button"
+                onClick={ this.savingUser }
+              >
+                Entrar
+              </button>
+            </div>
+          )}
+      </>
     );
   }
 }
